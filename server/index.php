d2f24c2638745813a142e0a1f36afc2c464fb106
Oscar <orn345629@gmail.com>
2026-06-17 20:59:29 -0600

    sz

--- server/index.php
+++ server/index.php
@@ -1,7 +1,5 @@
 <?php
-// server/index.php
 
-// 1. Headers necesarios para permitir CORS desde tu frontend de React
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
